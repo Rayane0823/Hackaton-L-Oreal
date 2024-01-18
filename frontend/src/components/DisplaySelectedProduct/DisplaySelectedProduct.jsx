d0540de4763d4a33bdb3980a3a5a705b63cd6487
ItsMatDev <thierry.matthieu@live.fr>
2024-01-18 23:28:07 +0100

    fixed selection card and search bar

--- frontend/src/components/DisplaySelectedProduct/DisplaySelectedProduct.jsx
+++ frontend/src/components/DisplaySelectedProduct/DisplaySelectedProduct.jsx
@@ -1,16 +1,20 @@
 import "./DisplaySelectedProduct.scss";
 import shampoo from "../../assets/shampoo.png";
+import { useGlobalContext } from "../Context/GlobalContextProvider";
 
 function DisplaySelectedProduct() {
+  const { selectedValue } = useGlobalContext();
   return (
-    <div className="selected_product_container">
-      <img
-        src={shampoo}
-        alt="Produit sélectionné"
-        className="selected_product_image"
-      />
-      <h1 className="selected_product_title">Shampooing 1</h1>
-    </div>
+    selectedValue && (
+      <div className="selected_product_container">
+        <img
+          src={shampoo}
+          alt="Produit sélectionné"
+          className="selected_product_image"
+        />
+        <h1 className="selected_product_title">Shampooing 1</h1>
+      </div>
+    )
   );
 }
 
