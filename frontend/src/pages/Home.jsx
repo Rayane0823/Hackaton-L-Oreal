5f16b872e5b66787f5b70cb11d421c657e0c21c0
Romain <98345704+Romlion@users.noreply.github.com>
2024-01-18 09:42:22 +0100

    NavBar style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import ProductCard from "../components/ProductCard/ProductCard";
+import NavBar from "../components/NavBar/NavBar";
+import "./Home.scss";
 
 function Home() {
   return (
     <div>
+      <NavBar />
       <ProductCard />
     </div>
   );
