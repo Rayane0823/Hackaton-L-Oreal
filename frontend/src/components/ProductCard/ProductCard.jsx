7101612420e21f18723a7c9594b055a128b4b736
ItsMatDev <thierry.matthieu@live.fr>
2024-01-18 23:54:21 +0100

    fixed button click on card and some double display for description

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -13,49 +13,43 @@ function ProductCard() {
       return newStates;
     });
   };
-  console.info(handleClick);
-
-  const [open, setIsOpen] = useState(false);
-
-  const hClick = () => {
-    setIsOpen(!open);
-  };
 
   return (
     <div className="all_card">
       {API &&
-        API.map((CardDecision, index) => {
-          return (
-            <div key={[index]} className="Card">
-              <h2 className="titlecard">{CardDecision.Titre}</h2>
-              <img className="imgcard" src="src/assets/0.png" alt="shampoing" />
+        API.map((CardDecision, index) => (
+          <div key={CardDecision.id} className="Card">
+            <h2 className="titlecard">{CardDecision.Titre}</h2>
+            <img className="imgcard" src="src/assets/0.png" alt="shampoing" />
 
-              <button className="button__desc" type="button" onClick={hClick}>
-                Plus de détails
-              </button>
-              <div className={open ? "desc" : "desc-hidden"}>
-                <p className="desc_text">
-                  <b>Type:</b> shampooing
-                </p>
-                <p className="desc_text">
-                  <b>Description: </b>Lorem ipsum dolor sit, amet consectetur
-                  adipisicing elit. Accusantium voluptatum culpa repudiandae!
-                  Pariatur odit temporibus.
-                </p>
-                <p className="desc_text">Shampoing pour cheveux secs</p>
-                <p className="price">20€</p>
-                {cardStates[index] && CardDecision.description}
-                <Link
-                  to="https://www.loreal-paris.fr/?&wiz_medium=cpc&wiz_source=google&wiz_campaign=oap_goog_ao_othr__bran_search_text_eg_fr__roas&gad_source=1&gclid=Cj0KCQiAtaOtBhCwARIsAN_x-3LZgHWOusXX-wpSeWR9TgvkjCsz_C2W31AcNbBxvAr0_etV7yQsJjgaAsguEALw_wcB&gclsrc=aw.ds"
-                  className="linkloreal"
-                  alt="loreal"
-                >
-                  Lien du site marchand
-                </Link>
-              </div>
+            <button
+              className="button__desc"
+              type="button"
+              onClick={() => handleClick(index)}
+            >
+              Plus de détails
+            </button>
+            <div className={cardStates[index] ? "desc" : "desc-hidden"}>
+              <p className="desc_text">
+                <b>Type:</b> shampooing
+              </p>
+              <p className="desc_text">
+                <b>Description: </b>Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Accusantium voluptatum culpa repudiandae!
+                Pariatur odit temporibus.
+              </p>
+              <p className="desc_text">Shampoing pour cheveux secs</p>
+              <p className="price">20€</p>
+              <Link
+                to="https://www.loreal-paris.fr/?&wiz_medium=cpc&wiz_source=google&wiz_campaign=oap_goog_ao_othr__bran_search_text_eg_fr__roas&gad_source=1&gclid=Cj0KCQiAtaOtBhCwARIsAN_x-3LZgHWOusXX-wpSeWR9TgvkjCsz_C2W31AcNbBxvAr0_etV7yQsJjgaAsguEALw_wcB&gclsrc=aw.ds"
+                className="linkloreal"
+                alt="loreal"
+              >
+                Lien du site marchand
+              </Link>
             </div>
-          );
-        })}
+          </div>
+        ))}
     </div>
   );
 }
